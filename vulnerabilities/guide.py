68e7cbdbbcee9a1081c4e0dc6142f36f89d7c663
JewishLewish <frostyornot@gmail.com>
2024-03-28 09:45:52 -0700

    laying out dir

---
+++ vulnerabilities/guide.py
@@ -0,0 +1,10 @@
+#guide.py
+#This part of the code retrieves all the kinds of vulnerabilities
+
+class DictionaryOfFlawed:
+    def __init__(self) -> None:
+        flawed = []
+        good = []
+    
+    def genPrompt(self) -> tuple:
+        return {}

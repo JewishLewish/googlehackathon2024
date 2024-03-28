03847c864ed5f6f10cdf8af4943c4d2aedb8c399
JewishLewish <frostyornot@gmail.com>
2024-03-28 14:21:46 -0700

    re-design

--- vulnerabilities/sql_injection/review.py
+++ vulnerabilities/sql_injection/review.py
@@ -1,18 +1,14 @@
 def flawed() -> tuple[str]:
     return (
-        (
-            '''cursor.execute(f"INSERT INTO users (name, age) VALUES ('{name}','{age}')")''', 
-            '''query = f"SELECT * FROM users WHERE id = '{user_id}'"''',
-            '''cursor.execute(f"INSERT INTO users (name, age) VALUES ("+name+","+age+")'''
-        )
-        )
+        '''cursor.execute(f"INSERT INTO users (name, age) VALUES ('{name}','{age}')")''', 
+        '''query = f"SELECT * FROM users WHERE id = '{user_id}'"''',
+        '''cursor.execute(f"INSERT INTO users (name, age) VALUES ("+name+","+age+")'''
+    )
 
 def fixed() -> tuple[str]:
     return (
-        (
-            '''cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))''',
-            '''query = "SELECT * FROM users WHERE id = %s"
+        '''cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))''',
+        '''query = "SELECT * FROM users WHERE id = %s"
 cursor.execute(query, (user_id,))''',
-            '''cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))'''
-        )
+        '''cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))'''
     )
